c4b1515536d2457b6f1e3c33c73a88ab630bdec1
Lonis <164430029+lonis752@users.noreply.github.com>
2025-03-28 17:20:01 -0700

    Created start to homepage with carousel and navbar.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,10 @@
+import { stripe } from '@/lib/stripe';
 import React from 'react'
 
-const ProductPage = () => {
+const ProductPage = async () => {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
   return (
     <div>ProductPage</div>
   )
